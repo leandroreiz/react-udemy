f09d43d0162217ff661ca924a57ca03e90915ed5
Leandro Reis <leandro.reiz@gmail.com>
2020-04-26 22:20:45 +0100

    Understanding the base features & syntax
    - Adding two way binding
    - Styling with stylesheets
    - Working with inline styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,19 +22,40 @@ class App extends Component {
     });
   };
 
+  nameChangeHandler = event => {
+    this.setState({
+      persons: [
+        { name: 'Leandro Reis', age: 36 },
+        { name: event.target.value, age: 34 },
+        { name: 'Nina Rodrigues Reis', age: 6 }
+      ]
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <h4>The paragraph below is a functional component</h4>
-        <button onClick={this.switchNameHandler.bind(this, 'theVacili')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, 'theVacili')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          clickMe={() => this.switchNameHandler('Lion')} >My hobbies: Reading</Person>
+          clickMe={() => this.switchNameHandler('Lion')} 
+          changeName={this.nameChangeHandler} >My hobbies: Reading</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-      <div>
+      <div className="Person">
         <p onClick={props.clickMe}>I'm {props.name} and I'm {props.age} years old!</p>
+        <input type="text" onChange={props.changeName} value={props.name} />
       </div>
     )
 };
