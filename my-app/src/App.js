4eb35ec3424f3271e67172f6c837d0b4a226e873
Leandro Reis <leandro.reiz@gmail.com>
2020-04-28 13:58:57 +0100

    Working with Lists & Conditionals
    - Rendering content conditionally
    - Handling dynamic content
    - Outputting Lists
    - Lists & state
    - Updating state immutably
    - Lists & keys
    - Flexible lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,31 +5,43 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Leandro', age: 36 },
-      { name: 'Chelem', age: 34 },
-      { name: 'Nina', age: 6 }
+      { id: 1, name: 'Leandro', age: 36 },
+      { id: 2, name: 'Chelem', age: 34 },
+      { id: 3, name: 'Nina', age: 6 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   };
 
-  switchNameHandler = newName => {
-    this.setState({
-      persons: [
-        { name: newName, age: 36 },
-        { name: 'Chelem Rodrigues', age: 34 },
-        { name: 'Nina Rodrigues Reis', age: 6 }
-      ]
-    });
-  };
+  // receives the event and the id as parameters
+  namechangeHandler = (event, id) => {
+    // retrieves the index of the person targeted
+    const personIndex = this.state.persons.findIndex(person => person.id === id);
+    // creates a copy of that person (to not directly mutate the state)
+    const person = {...this.state.persons[personIndex]};
+    // changes the name for that person
+    person.name = event.target.value;
+    // copies the array of persons
+    const persons = [...this.state.persons];
+    // update the targeted person with the object created above
+    persons[personIndex] = person;
+    // set the new state
+    this.setState({ persons: persons });
+  }
 
-  nameChangeHandler = event => {
-    this.setState({
-      persons: [
-        { name: 'Leandro Reis', age: 36 },
-        { name: event.target.value, age: 34 },
-        { name: 'Nina Rodrigues Reis', age: 6 }
-      ]
-    });
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons; // creates a pointer to the persons array [not recommended]
+    // const persons = this.state.persons.slice(); // copies the array into the persons variable [vanilla JavaScript]
+    const persons = [...this.state.persons]; // same as splice() but using the ES6 spread operator
+    // remove the person on that specific index
+    persons.splice(personIndex, 1);
+    // update the state
+    this.setState({ persons: persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   };
 
   render() {
@@ -41,24 +53,32 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              clickMe={this.deletePersonHandler.bind(this, index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={event => this.namechangeHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <h4>The paragraph below is a functional component</h4>
         <button
           style={style}
-          onClick={this.switchNameHandler.bind(this, 'theVacili')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          clickMe={() => this.switchNameHandler('Lion')} 
-          changeName={this.nameChangeHandler} >My hobbies: Reading</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
     return (
       <div className="Person">
         <p onClick={props.clickMe}>I'm {props.name} and I'm {props.age} years old!</p>
-        <input type="text" onChange={props.changeName} value={props.name} />
+        <input type="text" onChange={props.changed} value={props.name} />
       </div>
     )
 };
