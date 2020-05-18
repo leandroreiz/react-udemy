29cdb51f280ec209d6b9eb6b916fe8ef919ce3c0
Leandro Reis <leandro.reiz@gmail.com>
2020-05-18 01:08:40 +0100

    Reaching Out To The Web (Http & AJAX)
    - Understanding Http requests in React
    - Using Axios
    - Creating a Http request to GET data
    - Rendering fetched data to the screen
    - Transforming data
    - Fetching data on update (avoiding infinite loops/requests)

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -7,16 +7,50 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Leandro'
+                    }
+                });
+                this.setState({ posts: updatedPosts });
+                console.log(this.state.posts);
+            });
+    }
+
+    postSelectedHandler = id => {
+        this.setState({ selectedPostId: id });
+    }
+
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return (
+                <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            );
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
