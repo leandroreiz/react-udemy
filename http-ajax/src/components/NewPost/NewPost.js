b353be7cc9820967847d70c3f46c47aea377cc8d
Leandro Reis <leandro.reiz@gmail.com>
2020-05-18 13:02:28 +0100

    Reaching Out To The Web (Http & AJAX)
    - POSTing data to the server
    - Sending a DELETE request
    - Fixing a bug
    - Handling errors locally
    - Adding interceptors to execute code globally
    - Removing interceptors
    - Setting a default global configuration for Axios
    - Creating and using axios instances

--- http-ajax/src/components/NewPost/NewPost.js
+++ http-ajax/src/components/NewPost/NewPost.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
-        content: '',
-        author: 'Max'
+        body: '',
+        author: 'Leandro'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author
+        };
+        axios
+            .post('/posts', data)
+            .then(response => console.log(response));
     }
 
     render () {
@@ -16,13 +28,13 @@ class NewPost extends Component {
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.body} onChange={(event) => this.setState({body: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Max">Leandro</option>
+                    <option value="Manu">Chelem</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
