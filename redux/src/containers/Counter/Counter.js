242f0244d217d13a3417a55733629320dbebc744
Leandro Reis <leandro.reiz@gmail.com>
2020-06-01 13:37:03 +0100

    Immutable Update Patterns & Action Types
    - Updating Arrays Immutably
    - Outsourcing Action Types

--- redux/src/containers/Counter/Counter.js
+++ redux/src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
     render () {
@@ -19,7 +20,7 @@ class Counter extends Component {
                     {this.props.storedResults.map(storedResult => (
                         <li
                             key={storedResult.id}
-                            onClick={this.props.onDeleteResult}>{storedResult.value}</li>
+                            onClick={() => this.props.onDeleteResult(storedResult.id)}>{storedResult.value}</li>
                     ))}
                 </ul>
             </div>
@@ -36,12 +37,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD', value: 10}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 15}),
-        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddCounter: () => dispatch({type: actionTypes.ADD, value: 10}),
+        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 15}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElementId: id})
     };
 }
 
