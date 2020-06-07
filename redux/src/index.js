3a7112a1c73b5e65a66fa416b61b9c29f4a605ce
Leandro Reis <leandro.reiz@gmail.com>
2020-06-07 14:52:51 +0100

    Executing Asynchronous Code
    - Action Creators & Async Code
    - Handling Asynchronous Code
    - Restructuring Actions

--- redux/src/index.js
+++ redux/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
@@ -29,7 +30,7 @@ const logger = store => {
 // more on: https://github.com/zalmoxisus/redux-devtools-extension#installation
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
