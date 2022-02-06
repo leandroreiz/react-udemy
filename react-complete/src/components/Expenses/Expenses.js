0efcca9afec5bc7fb6393b1ee2c94b849300eed0
Leandro Reis <leandro.reiz@gmail.com>
2022-02-06 18:25:42 +0000

    Implementing the chart

--- react-complete/src/components/Expenses/Expenses.js
+++ react-complete/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import { useState } from "react";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setfilteredYear] = useState("2021");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList listData={filteredExpenses} selectedYear={filteredYear} />
     </Card>
   );
