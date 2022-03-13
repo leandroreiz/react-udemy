5a5eb362d2d06f5584d2375fced6ab03950c261d
Leandro Reis <leandro.reiz@gmail.com>
2022-03-13 13:57:14 +0000

    Refresher / Practice: Part 2/2

--- 17-advanced-redux/src/components/Cart/CartItem.js
+++ 17-advanced-redux/src/components/Cart/CartItem.js
@@ -1,7 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
+
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+  const { id, title, quantity, total, price } = props.item;
+
+  const addToCartHandler = () => {
+    dispatch(cartActions.addItemToCart(props.item));
+  };
+  const removeFromCartHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +28,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeFromCartHandler}>-</button>
+          <button onClick={addToCartHandler}>+</button>
         </div>
       </div>
     </li>
