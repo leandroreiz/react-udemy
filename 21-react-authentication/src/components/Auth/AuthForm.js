7ffc10779340866b9e91cecdec32c72021c57b30
Leandro Reis <leandro.reiz@gmail.com>
2022-03-31 22:02:13 +0100

    Adding user login

--- 21-react-authentication/src/components/Auth/AuthForm.js
+++ 21-react-authentication/src/components/Auth/AuthForm.js
@@ -24,38 +24,45 @@ const AuthForm = () => {
 
     setIsLoading(true);
 
-    if (isLogin) {
-    }
+    let url = '';
 
-    if (!isLogin) {
-      fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      ).then((res) => {
+    if (isLogin)
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+
+    if (!isLogin)
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
+
         if (res.ok) {
-          // ...
-        } else {
+          return res.json();
+        }
+
+        if (!res.ok) {
           res.json().then((data) => {
             let errorMessage = `Authentication failed!`;
-            // if (data && data.error && data.error.message)
-            //   errorMessage = data.error.message;
-
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
