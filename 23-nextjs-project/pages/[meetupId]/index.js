98164b75de881b10cd5d6dd55cf6b41fa4c69834
Leandro Reis <leandro.reiz@gmail.com>
2022-04-03 22:32:05 +0100

    Adding Custom Components & CSS Modules

---
+++ 23-nextjs-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1444623151656-030273ddb785?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      title="Guitar workshop"
+      address="10 Watermill Road, Raheny, Dublin"
+      description="A workshop for rock guitarists."
+    />
+  );
+};
+
+export default MeetupDetails;
--- 23-nextjs-project/pages/index.js
+++ 23-nextjs-project/pages/index.js
@@ -6,7 +6,7 @@ const DUMMY_MEETUPS = [
     title: 'Guitar workshop',
     image:
       'https://images.unsplash.com/photo-1444623151656-030273ddb785?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    address: '10b Watermill Road, Raheny, Dublin',
+    address: '10 Watermill Road, Raheny, Dublin',
     description: 'A workshop for rock guitarists.',
   },
 
